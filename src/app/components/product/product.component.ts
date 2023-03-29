3cc3fe3db74d0ccb5237ea84e220ea99462cdab3
furkaydin <furkay16@gmail.com>
2023-03-29 17:20:22 +0300

    basit api islemi yapildi.

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,16 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Product } from 'src/app/models/product';
+import { HttpClient } from '@angular/common/http';
+import { productResponseModel } from 'src/app/models/productResponseModel';
 
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
   styleUrls: ['./product.component.css']
 })
-export class ProductComponent {
+export class ProductComponent implements OnInit {
+  constructor(private httpClient: HttpClient) { }
+  products: Product[] = [];
+  apiUrl = "https://localhost:44314/api/products/getall";
 
 
-  products:Product[] = [
-  ];
+  ngOnInit(): void {
+    this.getProducts();
+  }
 
-  
+  getProducts() {
+    this.httpClient
+    .get<productResponseModel>(this.apiUrl)
+    .subscribe((response) => {
+      this.products = response.data
+    });
+  }
 }
