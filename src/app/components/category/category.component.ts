a404ec6b2e6f8efb08d2b2270f458bb4a0612424
furkaydin <furkay16@gmail.com>
2023-03-30 23:35:49 +0300

    category icin api istegi

--- src/app/components/category/category.component.ts
+++ src/app/components/category/category.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Category } from 'src/app/models/category';
+import { CategoryService } from 'src/app/services/category.service';
 
 @Component({
   selector: 'app-category',
   templateUrl: './category.component.html',
   styleUrls: ['./category.component.css']
 })
-export class CategoryComponent {
+export class CategoryComponent implements OnInit{
+  constructor(private categoryService:CategoryService){}
+ngOnInit(): void {
+  this.getCategories();
+}
+categories : Category[]=[];
 
+getCategories(){
+this.categoryService.getCategories().subscribe(response=>{
+  this.categories= response.data;
+})
+}
 }
